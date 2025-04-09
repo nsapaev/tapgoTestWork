bbacd152dfa7f62e0ff884c6f86f9ca83177d172
nsapaev <nsapaev5@gmail.com>
2025-04-10 01:43:36 +0500

    init app

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import {ref, onMounted } from "vue"
+
+const tasks = ref([])
+
+const loadTasks = async () => {
+  const saved = localStorage.getItem('tasks')
+  if (saved) {
+    tasks.value = JSON.parse(saved)
+  } else {
+    const response = await fetch('/tasks.json')
+    const data = await response.json()
+    tasks.value = data
+    localStorage.setItem('tasks', JSON.stringify(data))
+  }
+}
+onMounted(() => {
+  try {
+    loadTasks()
+  } catch (error) {
+    console.error('Ошибка загрузки данных:', error)
+  }
+})
+
+
+const toggleTask = (taskId) => {
+  const task = tasks.value.find(t => t.id === taskId)
+  if (task) {
+    task.done = !task.done
+    localStorage.setItem('tasks', JSON.stringify(tasks.value))
+  }
+}
+
+</script>
+
+<template>
+
+   <li v-for="task in tasks" :key="task.id">
+        <label > <input type="checkbox"  :checked="task.done"  @change="toggleTask(task.id)">  {{ task.title  }} </label> 
+    </li>
+     
+
+</template>
+
+
